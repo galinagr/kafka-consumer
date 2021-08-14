df680bd331f6f208d16bb3d0dc3997dc98175d7f
Galina Gryaznova <galina.gryaznova@scotts.com>
2021-08-14 22:26:59 +0300

    -consumer - integration test

---
+++ src/main/java/com/kafka/entity/Book.java
@@ -0,0 +1,23 @@
+package com.kafka.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+public class Book {
+    @Id
+    private Integer bookId;
+    private String bookName;
+    private String bookAuthor;
+    @OneToOne
+    @JoinColumn(name = "libraryEventId")
+    private LibraryEvent libraryEvent;
+}
